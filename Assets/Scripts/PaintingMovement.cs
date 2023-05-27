e9ce11ab21529e947f1d2a54b2f8fbbb5913872e
mij12 <oudeschoen13@gmail.com>
2023-05-27 16:53:09 +0200

    Added playermovement painting minigame

---
+++ Assets/Scripts/PaintingMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingMovement : MonoBehaviour
+{
+
+
+    public CharacterController controller;
+    public float speed = 13f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        controller.Move(move * Time.deltaTime * speed);
+
+        //Vector3 move = transform.right * x + transform.up * y;
+       
+    }
+}
